72e45a5cef24e026819e92db512962f26b4b9ac9
Dani <37778536+panovad@users.noreply.github.com>
2022-01-24 14:02:29 +0100

    Testing GitHub Actions

--- MoviesApp/MoviesApp/Models/NetworkManager.swift
+++ MoviesApp/MoviesApp/Models/NetworkManager.swift
@@ -87,7 +87,6 @@ class NetworkManager {
     func getData(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
         URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
     }
-    
 }
 
 class Reachability {
